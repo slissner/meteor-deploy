cafaa85d763effc6586126b1ababc4c239c78c23
Samuel Lissner <samuel@billymob.com>
2016-07-04 12:36:15 +0200

    feat(deploy) adds deploy environments, mongodb dump

--- sh/meteor-deploy.sh
+++ sh/meteor-deploy.sh
@@ -21,26 +21,57 @@
 ## Author:
 ## Samuel Lissner <http://www.slissner.de>
 
+## 6.
+##    a. Run with ./deploy-meteor.sh prod (to deploy to production)
+##    b. Run with ./deploy-meteor.sh (to deploy to staging)
+
 ## Variables
 
-APP_NAME="my-app"
-LOCAL_BUNDLE_DIR="bundle/"
-LOCAL_ARCHITECTURE="os.linux.x86_64"
+if [ "$1" = "prod" ]
+then
+    ############# Production ###########
+    DEPLOY_ENV="prod"
 
-SERVER_USER="root"
-SERVER_HOST="ip"
-SERVER_APP_ROOT_PATH="/opt"
-SERVER_APP_DIR_NAME="my-app"
-SERVER_MONGO_HOST="localhost"
-SERVER_MONGO_PORT="27017"
-SERVER_MONGO_DBNAME="app-dev"
-SERVER_PORT="3002"
-SERVER_ROOT_URL="http://my.host.name/"
+    APP_NAME="my-app"
+    APP_PROCESS_NAME="my-app-production"
+    LOCAL_BUNDLE_DIR="../mandabo-bundle/"
+    LOCAL_ARCHITECTURE="os.linux.x86_64"
+
+    SERVER_USER="root"
+    SERVER_HOST="ip"
+    SERVER_APP_ROOT_PATH="/opt"
+    SERVER_APP_DIR_NAME="my-app"
+    SERVER_MONGO_HOST="localhost"
+    SERVER_MONGO_PORT="27017"
+    SERVER_MONGO_DBNAME="app"
+    SERVER_PORT="3000"
+    SERVER_ROOT_URL="http://app.my.host.name/"
+else
+    ############# Staging ###########
+    DEPLOY_ENV="staging"
+
+    APP_NAME="my-app-dev"
+    APP_PROCESS_NAME="mandabo-staging"
+    LOCAL_BUNDLE_DIR="bundle/"
+    LOCAL_ARCHITECTURE="os.linux.x86_64"
+
+    SERVER_USER="root"
+    SERVER_HOST="ip"
+    SERVER_APP_ROOT_PATH="/opt"
+    SERVER_APP_DIR_NAME="my-app-staging"
+    SERVER_MONGO_HOST="localhost"
+    SERVER_MONGO_PORT="27017"
+    SERVER_MONGO_DBNAME="app-dev"
+    SERVER_PORT="3002"
+    SERVER_ROOT_URL="http://staging.my.host.name/"
+fi
 
 echo ""
 echo "############################"
 echo "## meteor deploy"
 echo "############################"
+echo ""
+echo "Deploy environment: ${DEPLOY_ENV}"
 echo ""
 echo "Deploy process of app $APP_NAME to $SERVER_HOST started"
 echo ""
@@ -54,6 +85,11 @@ scp ${LOCAL_BUNDLE_DIR}${APP_NAME}.tar.gz ${SERVER_USER}@${SERVER_HOST}:${SERVER
 
 # ssh
 ssh ${SERVER_USER}@${SERVER_HOST} "
+
+# do db backup
+cd ${SERVER_APP_ROOT_PATH};
+mongodump;
+echo 'Databases have been backed-up';
 
 # stop the app;
 cd ${SERVER_APP_ROOT_PATH}/${SERVER_APP_DIR_NAME};
